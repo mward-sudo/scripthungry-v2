def4ea7c1946cdf0aeaf1f40df72a23389ed626e
Michael Ward <mward82@gmail.com>
2021-11-01 20:52:12 +0000

    Image optimisations

--- next.config.js
+++ next.config.js
@@ -6,5 +6,6 @@ module.exports = withPreact({
   reactStrictMode: true,
   images: {
     formats: ['image/avif', 'image/webp'],
+    deviceSizes: [320, 400, 640, 750, 828, 1080, 1200, 1400, 1920, 2048, 3840],
   },
 })
