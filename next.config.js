07b8468e2faeaa44eee20779f71cb40f222df5c0
Michael Ward <mward82@gmail.com>
2021-11-03 00:00:25 +0000

    Reverts to react from preact

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const withPreact = require('next-plugin-preact')
-
-module.exports = withPreact({
+module.exports = {
   reactStrictMode: true,
   images: {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [320, 400, 640, 750, 828, 1080, 1200, 1400, 1920, 2048, 3840],
   },
-})
+}
