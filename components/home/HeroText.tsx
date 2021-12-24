11b3d60b9aaed1fab78686d34f52626a8774b18d
Michael Ward <mward82@gmail.com>
2021-12-24 02:16:45 +0000

    Use SVG image for homepage hero Fixes #41

--- components/home/HeroText.tsx
+++ components/home/HeroText.tsx
@@ -1,18 +1,23 @@
 import Image from 'next/image'
 
-import bg from '../../public/img/home/bg.png'
+import bg from '../../public/img/home/bg.svg'
 
 const HeroText = () => (
-  <section className="container relative max-w-3xl mx-auto text-center text-white text-shadow-xl">
-    <Image src={bg} layout="responsive" alt="" className="opacity-50" />
-    <div className="absolute top-0 bottom-0 left-0 right-0 flex flex-col justify-center py-4 mx-auto my-auto max-h-[100%]">
-      <div className="px-4 py-4 m-4 bg-black dark:bg-white dark:bg-opacity-20 sm:py-12 rounded-xl bg-opacity-20">
-        <h1 className="mb-4 text-3xl font-bold sm:mb-8 sm:text-5xl font-display">
-          Michael Ward
-        </h1>
-        <h2 className="text-3xl sm:text-5xl font-display">
-          Web Technology Expert
-        </h2>
+  <section className="container max-w-3xl mx-auto text-left text-white text-shadow-xl">
+    <div className="relative mx-4 sm:mx-8">
+      <Image
+        src={bg}
+        layout="responsive"
+        alt=""
+        className="hover:opacity-100 opacity-90 rounded-xl"
+      />
+      <div className="absolute top-0 bottom-0 left-0 flex flex-col justify-top py-4 mx-auto my-auto max-h-[100%]">
+        <div className="px-4 m-4">
+          <h1 className="pb-4 mb-2 text-2xl font-bold border-b-2 border-white sm:mb-4 sm:text-5xl font-display">
+            Michael Ward
+          </h1>
+          <h2 className="text-xl sm:text-4xl font-display">Web Tech Expert</h2>
+        </div>
       </div>
     </div>
   </section>
