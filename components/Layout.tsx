49cfe464358e4b3a89e4e6b52ae8854f1d9b56c8
Michael Ward <mward82@gmail.com>
2022-02-27 05:29:51 +0000

    Fix lint errors

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,12 +8,12 @@ import Footer from './Footer'
 const Layout: FC = ({ children }) => {
   return (
     <div className="w-full h-screen drawer">
-      <input id="my-drawer-3" type="checkbox" className="drawer-toggle"/> 
+      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
       <div className="flex flex-col drawer-content">
         <NavBar links={siteLinks} />
         <div className="container flex-1 px-3 mx-auto">{children}</div>
         <Footer />
-      </div> 
+      </div>
       <MobileDrawer links={siteLinks} />
     </div>
   )
--- components/nav/MobileDrawer.tsx
+++ components/nav/MobileDrawer.tsx
@@ -1,17 +1,21 @@
 import Link from 'next/link'
-import type {Links} from '../../models/siteLinks'
+import type { Links } from '../../models/siteLinks'
 
 type MobileDrawer = {
-  (props: {links: Links}): JSX.Element
+  (props: { links: Links }): JSX.Element
 }
 
-const MobileDrawer: MobileDrawer = ({links}) => (
+const MobileDrawer: MobileDrawer = ({ links }) => (
   <div className="drawer-side">
     <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
     <ul className="overflow-y-auto p-4 w-80 menu bg-base-100">
-      {links.map(({link, text}) => 
-        <li key={link}><Link href={link}><a>{text}</a></Link></li>
-      )}
+      {links.map(({ link, text }) => (
+        <li key={link}>
+          <Link href={link}>
+            <a>{text}</a>
+          </Link>
+        </li>
+      ))}
     </ul>
   </div>
 )
