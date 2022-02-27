524e5ec9a8d404931dcc8c4ccde2e2c107cc0b20
Michael Ward <mward82@gmail.com>
2022-02-27 05:21:31 +0000

    Implement navbar Fixes #59

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,17 +1,20 @@
-import SiteTitle from './SiteTitle'
 import type { FC } from 'react'
+import siteLinks from '../models/siteLinks'
+
+import NavBar from './nav/NavBar'
+import MobileDrawer from './nav//MobileDrawer'
 import Footer from './Footer'
 
 const Layout: FC = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen">
-      <header>
-        <div className="container px-3 py-4 mx-auto">
-          <SiteTitle />
-        </div>
-      </header>
-      <div className="container flex-1 px-3 mx-auto">{children}</div>
-      <Footer />
+    <div className="w-full h-screen drawer">
+      <input id="my-drawer-3" type="checkbox" className="drawer-toggle"/> 
+      <div className="flex flex-col drawer-content">
+        <NavBar links={siteLinks} />
+        <div className="container flex-1 px-3 mx-auto">{children}</div>
+        <Footer />
+      </div> 
+      <MobileDrawer links={siteLinks} />
     </div>
   )
 }
---
+++ models/siteLinks.ts
@@ -0,0 +1,16 @@
+export type Links = {
+  link: string,
+  text: string,
+}[]
+
+const links: Links = [
+  { link: '/showcase', text: 'Showcase' },
+  { link: '/blog', text: 'Blog' },
+  { link: 'https://github.com/mward-sudo/scripthungry-v2', text: 'GitHub' },
+  {
+    link: 'https://www.linkedin.com/in/michael-ward-ba003622/',
+    text: 'LinkedIn',
+  },
+]
+
+export default links
