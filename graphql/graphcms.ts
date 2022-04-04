25129dd2a0d10906169448f7b44806bb93185d07
Michael Ward <mward82@gmail.com>
2022-04-04 16:29:28 +0100

    GraphQL Code Generator setup for GraphCMS

---
+++ graphql/graphcms.ts
@@ -0,0 +1,148 @@
+import { gql } from '@apollo/client'
+
+export const graphCmsIndexPostsQuery = gql`
+  query IndexPosts($postsPerPage: Int!, $skip: Int!) {
+    posts(
+      first: $postsPerPage
+      stage: PUBLISHED
+      skip: $skip
+      orderBy: date_DESC
+    ) {
+      author {
+        name
+        twitterHandle
+        picture {
+          url(
+            transformation: { image: { resize: { height: 100, width: 100 } } }
+          )
+          height
+          width
+        }
+      }
+      excerpt
+      slug
+      title
+      content {
+        html
+      }
+      coverImage {
+        url
+        height
+        width
+      }
+    }
+    postsConnection(stage: PUBLISHED) {
+      aggregate {
+        count
+      }
+    }
+  }
+`
+
+export const graphCmsPostsTotalCountQuery = gql`
+  query PostsCount {
+    postsConnection(stage: PUBLISHED) {
+      aggregate {
+        count
+      }
+    }
+  }
+`
+
+export const graphCmsSlugsQuery = gql`
+  query Slugs {
+    posts(stage: PUBLISHED, orderBy: date_DESC, first: 1000) {
+      slug
+    }
+  }
+`
+
+export const grapgCmsPostBySlugQuery = gql`
+  query PostBySlug($slug: String!) {
+    post(where: { slug: $slug }, stage: PUBLISHED) {
+      author {
+        name
+        twitterHandle
+        picture {
+          url(
+            transformation: { image: { resize: { height: 100, width: 100 } } }
+          )
+          height
+          width
+        }
+      }
+      content {
+        html
+      }
+      coverImage {
+        url
+        height
+        width
+      }
+      date
+      excerpt
+      slug
+      title
+    }
+  }
+`
+
+export const graphCmsBlogCategoriesQuery = gql`
+  query Categories {
+    blogCategories(orderBy: name_ASC) {
+      slug
+      name
+      description {
+        html
+      }
+    }
+  }
+`
+
+export const graphCmsPostExcerptsByCategoryQuery = gql`
+  query PostExcerptsByCategory(
+    $slug: String!
+    $postsPerPage: Int!
+    $skip: Int!
+  ) {
+    blogCategory(where: { slug: $slug }) {
+      name
+      slug
+      description {
+        html
+      }
+      posts(orderBy: publishedAt_DESC, skip: $skip, first: $postsPerPage) {
+        author {
+          name
+          twitterHandle
+          picture {
+            height
+            width
+            url
+          }
+        }
+        coverImage {
+          url
+          height
+          width
+        }
+        excerpt
+        slug
+        title
+      }
+    }
+  }
+`
+
+export const graphCmsCategoryPostsCountQuery = gql`
+  query CategoryPostsCount($slug: String!) {
+    postsConnection(
+      where: { categories_some: { slug: $slug } }
+      stage: PUBLISHED
+    ) {
+      aggregate {
+        count
+      }
+    }
+  }
+`
--- pages/api/graphcms/graphql.ts
+++ pages/api/graphcms/graphql.ts
@@ -3,22 +3,21 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { proxyFetch } from './../../../lib/graphql-proxy'
 
 const envErrorMessage =
-  'Graph CMS passthrough endpoint not set. Set SERVER_GITHUB_ENDPOINT and API_GITHUB_TOKEN in the environment variables.'
+  'Graph CMS passthrough endpoint not set. Set GRAPHCMS_ENDPOINT and GRAPHCMS_TOKEN in the environment variables.'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   // Get the GraphCMS Endpoint from environment variables
-  const { SERVER_GITHUB_ENDPOINT, API_GITHUB_TOKEN } = process.env
+  const { GRAPHCMS_ENDPOINT, GRAPHCMS_TOKEN } = process.env
 
   // If no endpoint is set, throw an error
-  if (!SERVER_GITHUB_ENDPOINT || !API_GITHUB_TOKEN)
-    throw new Error(envErrorMessage)
+  if (!GRAPHCMS_ENDPOINT || !GRAPHCMS_TOKEN) throw new Error(envErrorMessage)
 
   // Use the current request to query the endpointUrl, adding the authKey as Authorization header
   const response = await proxyFetch({
     request: req,
     response: res,
-    endpointUrl: SERVER_GITHUB_ENDPOINT,
-    authKey: API_GITHUB_TOKEN,
+    endpointUrl: GRAPHCMS_ENDPOINT,
+    authKey: GRAPHCMS_TOKEN,
   })
 
   // Send the response back to the client
