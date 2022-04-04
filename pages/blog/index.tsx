c7f5e93790e8c4bc2855d24ccf7d188ee5499c5c
Michael Ward <mward82@gmail.com>
2022-04-04 17:16:49 +0100

    Blog Index page uses Apollo for categories data

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,6 +1,5 @@
-import type { ReactElement } from 'react'
 import type { GetStaticProps } from 'next'
-import type { iBlogCategories, iPostExcerpt } from '../../models/blog'
+import type { iPostExcerpt } from '../../models/blog'
 
 import SiteSettings from '../../lib/settings'
 import Layout from '../../components/Layout'
@@ -8,27 +7,24 @@ import PageTitle from '../../components/PageTitle'
 import PostExcerpt from '../../components/blog/PostExcerpt'
 import Pagination from '../../components/blog/Pagination'
 import { getTotalPostsNumber } from '../../models/blog'
-import {
-  calculateTotalIndexPages,
-  getIndexPosts,
-  getBlogCategories,
-} from '../../models/blog'
-import CategoryCloud from '../../components/blog/CategoryCloud'
+import { calculateTotalIndexPages, getIndexPosts } from '../../models/blog'
+import { CategoryCloud } from '../../components/blog/CategoryCloud'
 import Intro from '../../components/blog/SanitizedHtml'
+import { graphCmsClient } from '../../lib/apollo-client'
+import { graphCmsBlogCategoriesQuery } from '../../graphql/graphcms'
+import type { CategoriesQuery } from '../../generated/graphcms'
 
-interface index {
-  (props: {
-    title?: string
-    indexPosts: iPostExcerpt[]
-    pageNo: number
-    totalPages: number
-    categories: iBlogCategories
-    intro?: string
-    paginationPathBase?: string
-  }): ReactElement<any, any>
+type props = {
+  title?: string
+  indexPosts: iPostExcerpt[]
+  pageNo: number
+  totalPages: number
+  categories: CategoriesQuery['blogCategories']
+  intro?: string
+  paginationPathBase?: string
 }
 
-const index: index = ({
+const index = ({
   title = 'Blog',
   indexPosts,
   pageNo,
@@ -36,7 +32,7 @@ const index: index = ({
   categories,
   intro,
   paginationPathBase = '/blog',
-}) => (
+}: props) => (
   <Layout>
     <PageTitle>{title}</PageTitle>
     <div className="grid grid-cols-1 gap-x-4 md:grid-cols-4">
@@ -60,28 +56,31 @@ const index: index = ({
  * Gets blog post excerpts for static site generation
  */
 export const getStaticProps: GetStaticProps = async () => {
+  const categoriesResponse = await graphCmsClient.query<CategoriesQuery>({
+    query: graphCmsBlogCategoriesQuery,
+  })
+
   const postsPerPage = SiteSettings.POSTS_PER_PAGE
   const indexPosts = await getIndexPosts(1, postsPerPage)
   const totalPosts = await getTotalPostsNumber()
   const totalPages = calculateTotalIndexPages(totalPosts)
-  const categoriesData = await getBlogCategories()
 
   return {
     props: {
       indexPosts: indexPosts?.data?.posts,
       pageNo: 1,
       totalPages,
-      categories: categoriesData?.data?.blogCategories,
+      categories: categoriesResponse.data.blogCategories,
     },
     revalidate: 60,
   }
 }
 
-interface Categories {
-  (props: { categories: iBlogCategories }): ReactElement<any, any>
-}
-
-const Categories: Categories = ({ categories }) => (
+const Categories = ({
+  categories,
+}: {
+  categories: CategoriesQuery['blogCategories']
+}) => (
   <div className="hidden text-center md:block md:col-span-1">
     <h2 className="text-xl font-bold">Categories</h2>
     <CategoryCloud categories={categories} />
