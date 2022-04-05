9c940976bf79f3f3d0fb12e90b67adde70816026
Michael Ward <mward82@gmail.com>
2022-04-05 03:07:14 +0100

    Cleans up environment variable usage and removes redundant file

--- pages/api/graphcms/graphql.ts
+++ pages/api/graphcms/graphql.ts
@@ -3,20 +3,21 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { proxyFetch } from './../../../lib/graphql-proxy'
 
 const envErrorMessage =
-  'Graph CMS passthrough endpoint not set. Set GRAPHCMS_ENDPOINT and GRAPHCMS_TOKEN in the environment variables.'
+  'Graph CMS passthrough endpoint not set. Set SERVER_GRAPHCMS_ENDPOINT and GRAPHCMS_TOKEN in the environment variables.'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   // Get the GraphCMS Endpoint from environment variables
-  const { GRAPHCMS_ENDPOINT, GRAPHCMS_TOKEN } = process.env
+  const { SERVER_GRAPHCMS_ENDPOINT, GRAPHCMS_TOKEN } = process.env
 
   // If no endpoint is set, throw an error
-  if (!GRAPHCMS_ENDPOINT || !GRAPHCMS_TOKEN) throw new Error(envErrorMessage)
+  if (!SERVER_GRAPHCMS_ENDPOINT || !GRAPHCMS_TOKEN)
+    throw new Error(envErrorMessage)
 
   // Use the current request to query the endpointUrl, adding the authKey as Authorization header
   const response = await proxyFetch({
     request: req,
     response: res,
-    endpointUrl: GRAPHCMS_ENDPOINT,
+    endpointUrl: SERVER_GRAPHCMS_ENDPOINT,
     authKey: GRAPHCMS_TOKEN,
   })
 
